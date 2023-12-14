eba6b72c0ab6878929dc16069902371427241f0a
wilmer <will.devpower@gmail.com>
2023-12-14 11:15:10 -0400

    product link restored

--- app/assets/bundle.js
+++ app/assets/bundle.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-const API_URL = 'https://5eb5-38-51-157-55.ngrok-free.app'
+const API_URL = 'https://rivkacustomdiscounts.tech'
 const retrieveCartData = async () => {
   return await $.ajax({
     type: 'GET',
