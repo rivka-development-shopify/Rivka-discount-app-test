b91fb55d85d3a4b3df33944e9030c19f8cdde37b
Douglas Camargo <douglas@thecodesoup.com>
2023-12-14 17:14:38 -0300

    fix(bundle): solving merge errors

--- app/assets/bundle.js
+++ app/assets/bundle.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-const API_URL = 'https://114c-181-31-154-153.ngrok-free.app'
+const API_URL = 'https://rivkacustomdiscounts.tech'
 const retrieveCartData = async () => {
   return await $.ajax({
     type: 'GET',
