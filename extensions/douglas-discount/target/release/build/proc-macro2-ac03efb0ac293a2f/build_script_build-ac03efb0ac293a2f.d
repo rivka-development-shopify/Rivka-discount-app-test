8910d2cf44e62040a31cb8084bcea6a7f9e0e407
wilmer <will.devpower@gmail.com>
2023-12-15 12:35:58 -0400

    unnecessary extension folders deleted

--- extensions/douglas-discount/target/release/build/proc-macro2-ac03efb0ac293a2f/build_script_build-ac03efb0ac293a2f.d
+++
@@ -1,5 +0,0 @@
-/home/will_devpower/shopify-apps/discount-app-test-1/extensions/product-discount-rust/target/release/build/proc-macro2-ac03efb0ac293a2f/build_script_build-ac03efb0ac293a2f: /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.69/build.rs
-
-/home/will_devpower/shopify-apps/discount-app-test-1/extensions/product-discount-rust/target/release/build/proc-macro2-ac03efb0ac293a2f/build_script_build-ac03efb0ac293a2f.d: /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.69/build.rs
-
-/home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.69/build.rs:
--- extensions/douglas-discount/target/release/deps/thiserror-2700841e065b877b.d
+++
@@ -1,9 +0,0 @@
-/home/will_devpower/shopify-apps/discount-app-test-1/extensions/product-discount-rust/target/release/deps/thiserror-2700841e065b877b.rmeta: /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/lib.rs /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/aserror.rs /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/display.rs
-
-/home/will_devpower/shopify-apps/discount-app-test-1/extensions/product-discount-rust/target/release/deps/libthiserror-2700841e065b877b.rlib: /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/lib.rs /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/aserror.rs /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/display.rs
-
-/home/will_devpower/shopify-apps/discount-app-test-1/extensions/product-discount-rust/target/release/deps/thiserror-2700841e065b877b.d: /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/lib.rs /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/aserror.rs /home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/display.rs
-
-/home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/lib.rs:
-/home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/aserror.rs:
-/home/will_devpower/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.49/src/display.rs:
