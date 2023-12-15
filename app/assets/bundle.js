8c7b7d18c6e8e5d6abc448cc35ac3c4745ee8e73
Douglas Camargo <douglas@thecodesoup.com>
2023-12-14 23:22:36 -0300

    Squashed commit of the following:
    commit d0493abc5f07edaca8abcb1356f671139c6dbda3
    Author: Douglas Camargo <douglas@thecodesoup.com>
    Date:   Thu Dec 14 23:21:53 2023 -0300
    
        feature(bundle): fixing bugs and adding remove discount feature
    
    commit 7be638262da0385a9716fb0fe502403a388e8447
    Author: wilmer <will.devpower@gmail.com>
    Date:   Thu Dec 14 21:35:16 2023 -0400
    
        Refactor bundle.js
    
    commit b80ef04d351cf041adc14e55e06bd54882896f4a
    Merge: 57846f8 6f51e77
    Author: wilmer <will.devpower@gmail.com>
    Date:   Thu Dec 14 18:42:50 2023 -0400
    
        Merge branch 'main' into wr/cart-drawer-ui
    
    commit 57846f822db73ba2d17f5f22603e886181ce2628
    Author: wilmer <will.devpower@gmail.com>
    Date:   Thu Dec 14 10:58:28 2023 -0400
    
        UI handling completed: logic to handle all discount related UI in the cart drawer to the bundle
    
    commit ec1f8cd67d05dc6ef7ec0861856aa71b01301e94
    Merge: 0b88039 8eddba3
    Author: wilmer <will.devpower@gmail.com>
    Date:   Tue Dec 12 14:17:51 2023 -0400
    
        logs added
    
    commit 0b88039be170817ab922fd2de663973036d00afb
    Merge: bb3ee7f f6e55dd
    Author: wilmer <will.devpower@gmail.com>
    Date:   Tue Dec 12 13:31:24 2023 -0400
    
        Fix completed
    
    commit bb3ee7f620721c64f847797fc6b66e6e4c97a62a
    Merge: 8e531aa 626f0d9
    Author: wilmer <will.devpower@gmail.com>
    Date:   Tue Dec 12 11:07:03 2023 -0400
    
        Merge branch 'heroku' into wr/cart-drawer-ui
    
    commit 8e531aa570b3d71b0ed8b2274c564a8f925354b7
    Author: wilmer <will.devpower@gmail.com>
    Date:   Tue Dec 12 10:49:39 2023 -0400
    
        Logic to prevent page reloading upon adding discounts

--- app/assets/bundle.js
+++ app/assets/bundle.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-const API_URL = 'https://114c-181-31-154-153.ngrok-free.app'
+const API_URL = 'https://rivkacustomdiscounts.tech'
 const retrieveCartData = async () => {
   return await $.ajax({
     type: 'GET',
@@ -21,56 +21,17 @@ const parseCartData = (rawCartData) => {
   ) ?? [];}
 
 
-const applyDiscount = async (code, cartData) => {
-  try {
-    const paymentsConfigResponse = await fetch("/payments/config", {"method": "GET"})
-    const paymentsConfigData = await paymentsConfigResponse.json()
-    authorization_token = btoa(paymentsConfigData.paymentInstruments.accessToken)
-
-    let body = {"checkout": { "country": Shopify.country,"discount_code": code,"line_items": cartData.items, 'presentment_currency': Shopify.currency.active } }
-
-    const walletsCheckoutResponse = await fetch(
-      '/wallets/checkouts/',
-      {
-      "headers": {
-        "accept": "*/*", "cache-control": "no-cache",
-        "authorization": "Basic " + authorization_token,
-        "content-type": "application/json, text/javascript",
-        "pragma": "no-cache", "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin"
-      },
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "method": "POST", "mode": "cors", "credentials": "include",
-      "body": JSON.stringify(body)
-      }
-    )
-
-    const walletsCheckoutData = await walletsCheckoutResponse.json()
-    console.log(walletsCheckoutData.checkout);
-
-    if(walletsCheckoutData.checkout && walletsCheckoutData.checkout.applied_discounts.length > 0){
-      let discountApplyUrl = "/discount/"+code+"?v="+Date.now()+"&redirect=/checkout/";
-      fetch(discountApplyUrl, {}).then(function(response) { return response.text(); })
-      let localStorageValue = {
-        'code': code.trim(),
-        'totalCart': walletsCheckoutData.checkout.total_line_items_price
-      };
-      localStorage.setItem("discountCode", JSON.stringify(localStorageValue));
-    }else{
-      localStorage.removeItem("discountCode");
-    }
-    return true
-  } catch(e) {
-    console.error(e)
-    return null
-  }
+const removeDiscountCookie = async (code) => {
+  document.cookie = `discount_code=";path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT";`
 }
 
-const updateCartPrices = (discountResponse) => {
-  console.log('70', discountResponse)
+const addDiscountCookie = async (code) => {
+  document.cookie = `discount_code=${code}; path=/`;
+  return true;
 }
 
-const applyAndSave = async (listDiscountsData, cartData) => {
+
+const applyAndSave = async (listDiscountsData) => {
   const submitButton = document.getElementById('rivka-app-discount-code-submit');
   const errorMessage = document.querySelector('.stack-discounts-message');
   if(
@@ -78,22 +39,16 @@ const applyAndSave = async (listDiscountsData, cartData) => {
     &&
     (listDiscountsData?.body?.data?.productDiscountedPrices ?? null)
   ) {
-    if(await applyDiscount(
-      listDiscountsData.body.data.newTempDiscountInfo.code,
-      cartData
+    if(addDiscountCookie(
+      listDiscountsData.body.data.newTempDiscountInfo.code
     )) {
       const { body: { data: { newTempDiscountInfo, productDiscountedPrices } } } = listDiscountsData;
-      updateCartPrices({
-        productDiscountedPrices,
-        newTempDiscountInfo
-      })
       await localStorage.setItem('rivka-discount-applied', JSON.stringify({
         productDiscountedPrices,
         newTempDiscountInfo
       }));
-      setTimeout(() => {
-        updateCartDrawerUI(newTempDiscountInfo.code);
-      }, 2000);
+      const originalPrice = productDiscountedPrices.reduce((acm, discountInfo) => { return acm + discountInfo.price}, 0);
+      updateCartDrawerUI(newTempDiscountInfo.code, newTempDiscountInfo.amount, originalPrice);
     }
   } else {
     errorMessage.style.display = 'block';
@@ -121,7 +76,7 @@ const handleUpdateDiscount = async () => {
 
   const listDiscountsData = await listDiscountsResponse.json()
 
-  applyAndSave(listDiscountsData, cartData);
+  applyAndSave(listDiscountsData);
 }
 
 const handleApplyDiscount = async (e) => {
@@ -138,47 +93,38 @@ const handleApplyDiscount = async (e) => {
 
   const listDiscountsData = await listDiscountsResponse.json();
 
-  applyAndSave(listDiscountsData, cartData, e.target.id);
+  applyAndSave(listDiscountsData);
 }
 
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-});
+const handleRemoveDiscount = () => {
+  console.log('REMOVING DISOCUNTS')
+  localStorage.removeItem('rivka-discount-applied')
+  localStorage.removeItem('discountCode')
+  removeDiscountCookie()
+  updateCartDrawerUI('', 0.0, 0.0)
+}
 
-const updateTotalPrice = async () => {
-  const cartData = await retrieveCartData();
+const updateCartDrawerUI = (tempCode, discounted_price, original_price) => {
+  // CREATE ELEMENTS WITH VANILLA JS BASED ON TEMP_CODE
+  const discountAppliedDiv = document.querySelector('.discount_applied');
+  const discountTitleDiv = document.querySelector('.discount-title');
+  const dcWrapperDiv = document.querySelector('.dc_wrapper');
+  const discountCodesDiv = document.querySelector('.discount__codes');
+  const codeParts = tempCode.split("-");
+
   const input = document.querySelector('#rivka-app-discount-code-input')
   const subTotal = document.querySelector('.cart__item--subtotal');
   const drawerSubTotal = document.querySelector('.cart-drawer__item--subtotal');
   const submitButton = document.getElementById('rivka-app-discount-code-submit');
-  let finalPrice = cartData.original_total_price;
-  if(cartData.total_discount > 0) {
-    finalPrice = cartData.original_total_price - cartData.total_discount;
-  }
-  const formattedFinalprice = formatter.format(finalPrice / 100);
-  const formattedTotalPrice = formatter.format(cartData.original_total_price / 100);
 
   const priceDiv = document.createElement('div');
-  let htmlPrice = ``;
-  if(cartData.total_discount > 0) {
-    htmlPrice = `
-      <div>Subtotal</div>
-      <div>
-        <span class="striked-price">${formattedTotalPrice}</span>
-        <span>${formattedFinalprice}</span>
-      </div>
-    `
-  } else {
-    htmlPrice = `
-      <div>Subtotal</div>
-      <div>
-        <span>${formattedTotalPrice}</span>
-      </div>
-    `
-  }
+  let htmlPrice = `
+    <div>Subtotal</div>
+    <div>
+      <span class="striked-price">$${original_price.toFixed(2)}</span>
+      <span>$${(original_price - discounted_price).toFixed(2)}</span>
+    </div>
+  `;
 
   priceDiv.classList.add('cart-drawer__item--subtotal');
   priceDiv.innerHTML = htmlPrice;
@@ -187,15 +133,6 @@ const updateTotalPrice = async () => {
   input.value = '';
   submitButton.classList.remove('loading');
   submitButton.disabled = false;
-}
-const updateCartDrawerUI = async (tempCode) => {
-  const cartData = await retrieveCartData();
-
-  const discountAppliedDiv = document.querySelector('.discount_applied');
-  const discountTitleDiv = document.querySelector('.discount-title');
-  const dcWrapperDiv = document.querySelector('.dc_wrapper');
-  const discountCodesDiv = document.querySelector('.discount__codes');
-  const codeParts = tempCode.split("-");
 
   const codesDiv = document.createElement('div');
   codesDiv.classList.add('dc_wrapper');
@@ -205,31 +142,39 @@ const updateCartDrawerUI = async (tempCode) => {
       <path fill-opacity=".25" d="M10.835 2H16c1.105 0 2 .895 2 2v5.172c0 .53-.21 1.04-.586 1.414l-6.818 6.818c-.777.778-2.036.782-2.82.01l-5.166-5.1c-.786-.775-.794-2.04-.02-2.828.002 0 .003 0 .003-.002l6.82-6.89C9.79 2.214 10.3 2 10.835 2zM13.5 8c.828 0 1.5-.672 1.5-1.5S14.328 5 13.5 5 12 5.672 12 6.5 12.672 8 13.5 8z"></path>
     </svg>
     <span>${codeParts[1]}</span>
-    <div id="delete__discount">
+    <button id="delete__discount">
       <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="Menu / Close_SM">
         <path id="Vector" d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
         </g>
       </svg>
-    </div>
+    </button>
   `;
   codesDiv.innerHTML = htmlCodes;
 
-  const div = document.createElement('div');
-  const html = `
+  const discountedPriceRedTextDiv = document.createElement('div');
+  const discountedPriceRedTextDivHtml = `
   <div><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="tags" class="svg-inline--fa fa-tags fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M497.941 225.941L286.059 14.059A48 48 0 0 0 252.118 0H48C21.49 0 0 21.49 0 48v204.118a48 48 0 0 0 14.059 33.941l211.882 211.882c18.744 18.745 49.136 18.746 67.882 0l204.118-204.118c18.745-18.745 18.745-49.137 0-67.882zM112 160c-26.51 0-48-21.49-48-48s21.49-48 48-48 48 21.49 48 48-21.49 48-48 48zm513.941 133.823L421.823 497.941c-18.745 18.745-49.137 18.745-67.882 0l-.36-.36L527.64 323.522c16.999-16.999 26.36-39.6 26.36-63.64s-9.362-46.641-26.36-63.64L331.397 0h48.721a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882z"></path></svg>
-  <span>Discount applied</span></div><span>${formatter.format(cartData.total_discount / 100)}</span>
+  <span>Discount applied</span></div><span>$${discounted_price.toFixed(2)}</span>
   `;
 
-  div.classList.add('discount-title');
-  div.innerHTML = html;
-  setTimeout(() => {
-    updateTotalPrice();
-  }, 2000);
-  discountCodesDiv?.appendChild(codesDiv);
+  discountedPriceRedTextDiv.classList.add('discount-title');
+  discountedPriceRedTextDiv.innerHTML = discountedPriceRedTextDivHtml;
+
   discountTitleDiv?.remove();
   dcWrapperDiv?.remove();
-  await discountAppliedDiv.appendChild(div);
+
+  if(localStorage.getItem('rivka-discount-applied')) {
+    discountCodesDiv?.appendChild(codesDiv);
+    discountAppliedDiv.appendChild(discountedPriceRedTextDiv);
+    document.getElementById('delete__discount').onclick = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      handleRemoveDiscount();
+    }
+  }
+
+  switchCartSubtotal()
 }
 
 const createForm = () => {
@@ -254,45 +199,55 @@ const createForm = () => {
   div.appendChild(button)
 }
 
+const switchCartSubtotal = () => {
+  if(!localStorage.getItem('rivka-discount-applied')) {
+    document.querySelector('.cart-drawer__item--subtotal').style.display = 'none';
+    document.querySelector('.cart__item--subtotal').style.display = 'flex';
+    return false
+  } else {
+    document.querySelector('.cart-drawer__item--subtotal').style.display = 'flex';
+    document.querySelector('.cart__item--subtotal').style.display = 'none';
+    return true
+  }
+};
+
+const onCartChange = () => {
+  if(switchCartSubtotal()) {
+    handleUpdateDiscount()
+  }
+  return
+}
+
 const observeCartChanges = () => {
   const cartObserver = new PerformanceObserver((list) => {
     list.getEntries().forEach((entry) => {
       const isValidRequestType = ['xmlhttprequest', 'fetch'].includes(entry.initiatorType);
       const isCartChangeRequest = /\/cart\//.test(entry.name);
       if (isValidRequestType && isCartChangeRequest) {
-        handleUpdateDiscount()
+        onCartChange()
       }
     });
   });
   cartObserver.observe({ entryTypes: ["resource"] });
 }
 
-
-createForm()
-observeCartChanges()
-
-/* if(localStorage.getItem('openCart')) {
-
-  document.querySelector('cart-drawer').classList.add('animate', 'active')
-  localStorage.setItem('openCart', false)
-} */
-
 // Function to update the UI based on the localStorage data
 const updateUIFromLocalStorage = async () => {
-  const cartData = await retrieveCartData()
   const listDiscountsData = JSON.parse(localStorage.getItem('rivka-discount-applied'));
   const discountInfo = await listDiscountsData?.newTempDiscountInfo;
+  const productDiscountedPrices = await listDiscountsData?.productDiscountedPrices;
 
-  if(cartData.total_discount === 0) {
-    return updateTotalPrice();
-  }
-  if (discountInfo) {
-    await updateCartDrawerUI(discountInfo.code);
+  if (discountInfo && productDiscountedPrices) {
+    const originalPrice = productDiscountedPrices.reduce((acm, discountInfo) => { return acm + discountInfo.price}, 0);
+    updateCartDrawerUI(discountInfo.code, discountInfo.amount, originalPrice);
+    switchCartSubtotal()
   }
 };
 
 // Update the UI when the page loads
-updateUIFromLocalStorage();
+window.addEventListener("load", (event) => {
+  updateUIFromLocalStorage();
+});
 
 // Update the UI when the localStorage changes
 window.addEventListener('storage', async (event) => {
@@ -301,4 +256,6 @@ window.addEventListener('storage', async (event) => {
   }
 });
 
-console.log('Custom Discounts (Rivka): Loaded')
+createForm()
+observeCartChanges()
+console.log('Custom Discounts (Rivka): Loaded');
